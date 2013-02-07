dd18348b9166ba32ebe950cb8466e23f2d6ea47a
asud16 <asud@mit.edu>
2013-02-07 15:13:09 -0500

    Update chap6.py

--- chap6.py
+++ chap6.py
@@ -1,9 +1,37 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Aparna Sud 
 
 
 # Ex. 6.6
 
+def first(word): 
+  return word[0]
+
+def last(word):
+	return word[-1]
+
+def middle(word):
+	return word[1:-1]
 
 
-# Ex 6.8
+# If you call a string with 2 letters, 1 letter, or a blank string '', you get '' as an output because it reads the middle letter as blank since it's not present 
+
+def is_palindrome(word):
+        if len(word) <=1:
+                return "Palindrome"
+        if first(word) == last(word):
+               return is_palindrome(middle(word))
+        else:
+                return "Not palindrome"
+# Ex. 6.8
+
+def gcd(a,b):
+        if b == 0:
+                return a 
+        else:
+                r = a%b
+                return gcd(b,r)
+    
+
+
+

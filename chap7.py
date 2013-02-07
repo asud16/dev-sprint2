20a9a9f5da5bfbaccbe988b419643ed41f4281c7
asud16 <asud@mit.edu>
2013-02-07 16:19:39 -0500

    Update chap7.py

--- chap7.py
+++ chap7.py
@@ -1,9 +1,20 @@
-# Enter your answrs for chapter 7 here
-# Name:
+# Enter your answers for chapter 7 here
+# Name: Aparna Sud 
 
 
 # Ex. 7.5
+import math
 
+def estimate_pi():
+    k = 0
+    while True:
+        z = ([math.factorial(4*k)*(1103+26390*k)]/[math.factorial(k)**4*396**(4*k)]
+        est += z 
 
+        if float(est) < 1e-15: 
+             break
+    return sum(est)* (2*sqrt(2))/9801
+        
 
-# How many iterations does it take to converge?
+# How many iterations does it take to converge? 
+# Can get the code to run ... invalid syntax as an error and not sure what's wrong 
